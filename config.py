83ae88fde07d6fbe5e769d01c6981956e0bdfb8e
ds22m013 <ds22m013@technikum-wien.at>
2022-12-27 07:26:22 +0100

    update new version

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-
-
 postgres_host='localhost'
 postgres_port='5432'
+postgres_database = 'dsi_postgres'
 postgres_user = 'postgres'
 postgres_password = 'DSIholiday'
+postgres_connection_string = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(postgres_user, postgres_password, postgres_host, postgres_port, postgres_database)
